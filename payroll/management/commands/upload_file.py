b617b96c9154ab8a3d12b6fd40294294cd232493
Crystal Faqiri <cfaqiri@gmail.com>
2022-02-17 20:44:57 -0500

    Commented work in progress status

--- payroll/management/commands/upload_file.py
+++ payroll/management/commands/upload_file.py
@@ -1,7 +1,7 @@
 import argparse
 from django.core.management.base import BaseCommand, CommandError
 
-
+# This is a work in progress
 class Command(BaseCommand):
     help = 'Import file at command line'
 
